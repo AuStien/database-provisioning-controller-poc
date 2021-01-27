a8e637c868a0b9554f6f9086b276ffbd79d62d31
Audun Stien <auduns@stacc.com>
2021-01-27 12:55:36 +0100

    Prepare for migration

--- controllers/database_controller.go
+++ controllers/database_controller.go
@@ -18,19 +18,23 @@ package controllers
 
 import (
 	"context"
-	"fmt"
-	"strings"
 	"time"
 
 	databasev1alpha1 "flow.stacc.dev/database-provisioning-poc/api/v1alpha1"
+	postgres "flow.stacc.dev/database-provisioning-poc/pkg/db"
 	kubernetes "flow.stacc.dev/database-provisioning-poc/pkg/kubernetes"
 	"github.com/go-logr/logr"
-	"github.com/jackc/pgx/v4/pgxpool"
+
+	// "github.com/golang-migrate/migrate/v4/database/postgres"
+	// _ "github.com/golang-migrate/migrate/v4/source/file"
+	// _ "github.com/jackc/pgx"
+
 	"github.com/sethvargo/go-password/password"
 	coreV1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	k8s "k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -38,9 +42,10 @@ import (
 // DatabaseReconciler reconciles a Database object
 type DatabaseReconciler struct {
 	client.Client
-	Log              logr.Logger
-	Scheme           *runtime.Scheme
-	KubernetesClient *kubernetes.Client
+	Log                 logr.Logger
+	Scheme              *runtime.Scheme
+	KubernetesClient    *kubernetes.Client
+	KubernetesClientset *k8s.Clientset
 }
 
 // +kubebuilder:rbac:groups=database.stacc.com,resources=databases,verbs=get;list;watch;create;update;patch;delete
@@ -74,32 +79,60 @@ func (r *DatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return ctrl.Result{}, nil
 	}
 
-	// Get k8s clientset
-	clientset, err := kubernetes.NewClientset(r.KubernetesClient)
-	if err != nil {
-		log.Error(err, "Error obtaining clientset")
-		return ctrl.Result{}, client.IgnoreNotFound(err)
-	}
-
 	// Get secret with database server password
-	serverSecret, err := clientset.CoreV1().Secrets(databaseServer.GetNamespace()).Get(databaseServer.Spec.SecretName, metav1.GetOptions{})
+	serverSecret, err := r.KubernetesClientset.CoreV1().Secrets(databaseServer.GetNamespace()).Get(databaseServer.Spec.SecretName, metav1.GetOptions{})
 	if err != nil {
 		log.Error(err, "Error obtaining secret")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	// Create url for inital connect to server
-	url := fmt.Sprintf("postgresql://%s:%s@%s:%d/postgres", databaseServer.Spec.Postgres.Username, serverSecret.Data["password"], databaseServer.Spec.Postgres.Host, databaseServer.Spec.Postgres.Port)
+	// // Create url for inital connect to server
+	// url := fmt.Sprintf("postgresql://%s:%s@%s:%d/postgres", databaseServer.Spec.Postgres.Username, serverSecret.Data["password"], databaseServer.Spec.Postgres.Host, databaseServer.Spec.Postgres.Port)
 
-	log.Info("Inital connect to database", "username", databaseServer.Spec.Postgres.Username, "database", "postgres", "host", databaseServer.Spec.Postgres.Host, "port", databaseServer.Spec.Postgres.Port)
+	// log.Info("Inital connect to database", "username", databaseServer.Spec.Postgres.Username, "database", "postgres", "host", databaseServer.Spec.Postgres.Host, "port", databaseServer.Spec.Postgres.Port)
 
-	// Create connection to server
-	dbpool, err := pgxpool.Connect(ctx, url)
-	if err != nil {
-		log.Info("Unable to connect to database")
+	// // Create connection to server
+	// conn, err := pgx.Connect(ctx, url)
+	// if err != nil {
+	// 	log.Info("Unable to connect to database")
+	// 	return ctrl.Result{RequeueAfter: time.Minute}, nil
+	// }
+	// defer conn.Close(ctx)
+
+	ps := postgres.PostgresServer{
+		Host: databaseServer.Spec.Postgres.Host,
+		Port: databaseServer.Spec.Postgres.Port,
+	}
+
+	if msg, err := ps.Connect(databaseServer.Spec.Postgres.Username, string(serverSecret.Data["password"])); err != nil {
+		log.Info(msg, "err", err)
 		return ctrl.Result{RequeueAfter: time.Minute}, nil
 	}
-	defer dbpool.Close()
+
+	//////// Connect to server /////////////////////////7
+
+	// db, err := sql.Open("postgres", url)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+	// 	os.Exit(1)
+	// }
+	// defer db.Close()
+
+	// driver, err := postgres.WithInstance(db, &postgres.Config{})
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "error getting driver: %v\n", err)
+	// 	os.Exit(1)
+	// }
+	// m, err := migrate.NewWithDatabaseInstance(
+	// 	"file:///home/a/utvikling/testing/go/db/migrations",
+	// 	"postgres", driver)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "migration failed: %v\n", err)
+	// 	os.Exit(1)
+	// }
+	// m.Steps(1)
+
+	///////////////////////////////////////////////////7
 
 	// Get username, set to database name if not present
 	username := database.Spec.Username
@@ -111,15 +144,25 @@ func (r *DatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	if !database.ObjectMeta.DeletionTimestamp.IsZero() && database.Spec.Deletable {
 		log.Info("Database being finalized")
 
-		_, err = dbpool.Exec(ctx, fmt.Sprintf("DROP DATABASE \"%s\"", database.Spec.Name))
-		if err != nil {
-			log.Error(err, "unable to drop database in database server")
+		// _, err = conn.Exec(ctx, fmt.Sprintf("DROP DATABASE \"%s\"", database.Spec.Name))
+		// if err != nil {
+		// 	log.Error(err, "unable to drop database in database server")
+		// 	return ctrl.Result{}, err
+		// }
+
+		if msg, err := ps.DeleteDatabase(); err != nil {
+			log.Error(err, msg)
 			return ctrl.Result{}, err
 		}
 
-		_, err = dbpool.Exec(ctx, fmt.Sprintf("DROP USER \"%s\"", username))
-		if err != nil {
-			log.Error(err, "unable to drop user in database server")
+		// _, err = conn.Exec(ctx, fmt.Sprintf("DROP USER \"%s\"", username))
+		// if err != nil {
+		// 	log.Error(err, "unable to drop user in database server")
+		// 	return ctrl.Result{}, err
+		// }
+
+		if msg, err := ps.DeleteUser(); err != nil {
+			log.Error(err, msg)
 			return ctrl.Result{}, err
 		}
 
@@ -140,7 +183,7 @@ func (r *DatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	}
 
 	// Check if database secret exists
-	dbSecret, err := clientset.CoreV1().Secrets(database.Spec.Secret.Namespace).Get(database.Spec.Secret.Name, metav1.GetOptions{})
+	dbSecret, err := r.KubernetesClientset.CoreV1().Secrets(database.Spec.Secret.Namespace).Get(database.Spec.Secret.Name, metav1.GetOptions{})
 	if err != nil {
 		// If error is other than "Not found" stop reconsiling
 		if !errors.IsNotFound(err) {
@@ -158,7 +201,7 @@ func (r *DatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 				"password": []byte(password),
 			},
 		}
-		_, err := clientset.CoreV1().Secrets(database.Spec.Secret.Namespace).Create(dbSecret)
+		_, err := r.KubernetesClientset.CoreV1().Secrets(database.Spec.Secret.Namespace).Create(dbSecret)
 		if err != nil {
 			log.Error(err, "unable to create secret")
 			return ctrl.Result{}, err
@@ -167,7 +210,7 @@ func (r *DatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	} else {
 		dbSecret.Data["username"] = []byte(username)
 		dbSecret.Data["password"] = []byte(password)
-		_, err := clientset.CoreV1().Secrets(database.Spec.Secret.Namespace).Update(dbSecret)
+		_, err := r.KubernetesClientset.CoreV1().Secrets(database.Spec.Secret.Namespace).Update(dbSecret)
 		if err != nil {
 			log.Error(err, "unable to update secret")
 			return ctrl.Result{}, err
@@ -179,23 +222,31 @@ func (r *DatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return ctrl.Result{}, err
 	}
 
-	// Check if user exists on server
-	commandTag, err := dbpool.Exec(ctx, fmt.Sprintf("SELECT usename FROM pg_user WHERE usename='%s'", username))
-	// If user doesn't exist create new
-	if err != nil || commandTag.RowsAffected() == 0 {
-		_, err = dbpool.Exec(ctx, fmt.Sprintf("CREATE USER \"%s\" WITH PASSWORD '%s'", username, password))
-		if err != nil {
-			log.Error(err, "unable to create role in database")
-			return ctrl.Result{}, err
-		}
-		// If user exists update password
-	} else {
-		_, err = dbpool.Exec(ctx, fmt.Sprintf("ALTER USER \"%s\" WITH PASSWORD '%s'", username, password))
-		if err != nil {
-			log.Error(err, "unable to alter user in database")
-			return ctrl.Result{}, err
-		}
+	// // Check if user exists on server
+	// commandTag, err := conn.Exec(ctx, fmt.Sprintf("SELECT usename FROM pg_user WHERE usename='%s'", username))
+	// // If user doesn't exist create new
+	// if err != nil || commandTag.RowsAffected() == 0 {
+	// 	_, err = conn.Exec(ctx, fmt.Sprintf("CREATE USER \"%s\" WITH PASSWORD '%s'", username, password))
+	// 	if err != nil {
+	// 		log.Error(err, "unable to create role in database")
+	// 		return ctrl.Result{}, err
+	// 	}
+	// 	// If user exists update password
+	// } else {
+	// 	_, err = conn.Exec(ctx, fmt.Sprintf("ALTER USER \"%s\" WITH PASSWORD '%s'", username, password))
+	// 	if err != nil {
+	// 		log.Error(err, "unable to alter user in database")
+	// 		return ctrl.Result{}, err
+	// 	}
+	// }
+
+	ps.Postgres = postgres.Postgres{Name: database.Spec.Name, Username: username, Password: password}
+
+	if msg, err := ps.CreateUser(); err != nil {
+		log.Error(err, msg)
+		return ctrl.Result{}, err
 	}
+
 	database.Status.User = "done"
 	if err := r.Status().Update(ctx, &database); err != nil {
 		log.Error(err, "unable to update database status")
@@ -203,15 +254,21 @@ func (r *DatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	}
 
 	// Try to create database
-	_, err = dbpool.Exec(ctx, fmt.Sprintf("CREATE DATABASE \"%s\" TEMPLATE \"template0\"", database.Spec.Name))
-	if err != nil {
-		if strings.Contains(err.Error(), "already exists") {
-			log.Info("Database already exisis")
-		} else {
-			log.Error(err, "unable to create database in database server")
-			return ctrl.Result{}, err
-		}
+	// _, err = conn.Exec(ctx, fmt.Sprintf("CREATE DATABASE \"%s\" TEMPLATE \"template0\"", database.Spec.Name))
+	// if err != nil {
+	// 	if strings.Contains(err.Error(), "already exists") {
+	// 		log.Info("Database already exisis")
+	// 	} else {
+	// 		log.Error(err, "unable to create database in database server")
+	// 		return ctrl.Result{}, err
+	// 	}
+	// }
+
+	if msg, err := ps.CreateDatabase(); err != nil {
+		log.Error(err, msg)
+		return ctrl.Result{}, err
 	}
+
 	database.Status.DB = "done"
 	if err := r.Status().Update(ctx, &database); err != nil {
 		log.Error(err, "unable to update database status")
@@ -219,25 +276,47 @@ func (r *DatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	}
 
 	// Grant permissions to user
-	_, err = dbpool.Exec(ctx, fmt.Sprintf("GRANT ALL ON DATABASE \"%s\" TO \"%s\"", database.Spec.Name, username))
-	if err != nil {
-		log.Error(err, "unable to grant permissions in database")
+	// _, err = conn.Exec(ctx, fmt.Sprintf("GRANT ALL ON DATABASE \"%s\" TO \"%s\"", database.Spec.Name, username))
+	// if err != nil {
+	// 	log.Error(err, "unable to grant permissions in database")
+	// 	return ctrl.Result{}, err
+	// }
+
+	if msg, err := ps.GrantPermissions(); err != nil {
+		log.Error(err, msg)
 		return ctrl.Result{}, err
 	}
+
 	database.Status.Permissions = "done"
 	if err := r.Status().Update(ctx, &database); err != nil {
 		log.Error(err, "unable to update database status")
 		return ctrl.Result{}, err
 	}
 
-	// Test if connection to new database works
-	newURL := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s", username, password, databaseServer.Spec.Postgres.Host, databaseServer.Spec.Postgres.Port, database.Spec.Name)
+	// // Test if connection to new database works
+	// newURL := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s", username, password, databaseServer.Spec.Postgres.Host, databaseServer.Spec.Postgres.Port, database.Spec.Name)
 
-	log.Info("Connecting to new database", "username", username, "database", database.Spec.Name, "host", databaseServer.Spec.Postgres.Host, "port", databaseServer.Spec.Postgres.Port)
+	// log.Info("Connecting to new database", "username", username, "database", database.Spec.Name, "host", databaseServer.Spec.Postgres.Host, "port", databaseServer.Spec.Postgres.Port)
 
-	newDBpool, err := pgxpool.Connect(ctx, newURL)
-	if err != nil {
-		log.Info("Unable to connect to new database", "err", err)
+	// newconn, err := pgxpool.Connect(ctx, newURL)
+	// if err != nil {
+	// 	log.Info("Unable to connect to new database", "err", err)
+	// 	database.Status.Connection = false
+	// 	if err := r.Status().Update(ctx, &database); err != nil {
+	// 		log.Error(err, "unable to update database status")
+	// 		return ctrl.Result{}, err
+	// 	}
+	// } else {
+	// 	database.Status.Connection = true
+	// 	if err := r.Status().Update(ctx, &database); err != nil {
+	// 		log.Error(err, "unable to update database status")
+	// 		return ctrl.Result{}, err
+	// 	}
+	// }
+	// defer newconn.Close()
+
+	if msg, err := ps.TestNewConnection(); err != nil {
+		log.Error(err, msg)
 		database.Status.Connection = false
 		if err := r.Status().Update(ctx, &database); err != nil {
 			log.Error(err, "unable to update database status")
@@ -250,7 +329,15 @@ func (r *DatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 			return ctrl.Result{}, err
 		}
 	}
-	defer newDBpool.Close()
+
+	if database.Spec.MigrationURL != "" {
+		if msg, err := ps.Migrate(database.Spec.MigrationURL); err != nil {
+			log.Error(err, msg)
+			return ctrl.Result{}, err
+		}
+	}
+
+	defer ps.Disconnect()
 
 	return ctrl.Result{}, nil
 }
--- controllers/databaseserver_controller.go
+++ controllers/databaseserver_controller.go
@@ -18,32 +18,34 @@ package controllers
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/go-logr/logr"
-	"github.com/jackc/pgx/v4/pgxpool"
 	"k8s.io/apimachinery/pkg/runtime"
+	k8s "k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	databasev1alpha1 "flow.stacc.dev/database-provisioning-poc/api/v1alpha1"
+	postgres "flow.stacc.dev/database-provisioning-poc/pkg/db"
 	kubernetes "flow.stacc.dev/database-provisioning-poc/pkg/kubernetes"
 )
 
 // DatabaseServerReconciler reconciles a DatabaseServer object
 type DatabaseServerReconciler struct {
 	client.Client
-	Log              logr.Logger
-	Scheme           *runtime.Scheme
-	KubernetesClient *kubernetes.Client
+	Log                 logr.Logger
+	Scheme              *runtime.Scheme
+	KubernetesClient    *kubernetes.Client
+	KubernetesClientset *k8s.Clientset
 }
 
 // +kubebuilder:rbac:groups=database.stacc.com,resources=databaseservers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=database.stacc.com,resources=databaseservers/status,verbs=get;update;patch
 
+// Reconcile DatabaseServer
 func (r *DatabaseServerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
 	log := r.Log.WithValues("databaseserver", req.NamespacedName)
@@ -54,30 +56,28 @@ func (r *DatabaseServerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, err
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	clientset, err := kubernetes.NewClientset(r.KubernetesClient)
-	if err != nil {
-		log.Error(err, "Error obtaining clientset")
-		return ctrl.Result{}, client.IgnoreNotFound(err)
-	}
-
-	secret, err := clientset.CoreV1().Secrets(databaseServer.GetNamespace()).Get(databaseServer.Spec.SecretName, metav1.GetOptions{})
+	secret, err := r.KubernetesClientset.CoreV1().Secrets(databaseServer.GetNamespace()).Get(databaseServer.Spec.SecretName, metav1.GetOptions{})
 	if err != nil {
 		log.Error(err, "Error obtaining secret")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-	url := fmt.Sprintf("postgresql://%s:%s@%s:%d/postgres", databaseServer.Spec.Postgres.Username, secret.Data["password"], databaseServer.Spec.Postgres.Host, databaseServer.Spec.Postgres.Port)
 
-	dbpool, err := pgxpool.Connect(ctx, url)
-	if err != nil {
+	server := postgres.PostgresServer{
+		Host: databaseServer.Spec.Postgres.Host,
+		Port: databaseServer.Spec.Postgres.Port,
+	}
+
+	if msg, err := server.Connect(databaseServer.Spec.Postgres.Username, string(secret.Data["password"])); err != nil {
+		log.Error(err, msg)
 		databaseServer.Status.Connected = false
-		if err1 := r.Status().Update(ctx, &databaseServer); err1 != nil {
+		if err := r.Status().Update(ctx, &databaseServer); err != nil {
 			log.Error(err, "unable to update databaseServer status")
-			return ctrl.Result{}, err1
+			return ctrl.Result{}, err
 		}
-		log.Info("Unable to connect to database")
 		return ctrl.Result{RequeueAfter: time.Minute}, nil
 	}
-	defer dbpool.Close()
+
+	defer server.Disconnect()
 
 	log.Info("Successfully connected to database")
 	databaseServer.Status.Connected = true
@@ -89,6 +89,7 @@ func (r *DatabaseServerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, err
 	return ctrl.Result{RequeueAfter: time.Minute}, nil
 }
 
+// SetupWithManager for DatabaseServer
 func (r *DatabaseServerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&databasev1alpha1.DatabaseServer{}).
