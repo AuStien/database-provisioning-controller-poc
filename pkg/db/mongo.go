dd5c152e6dc2ca55c8b57b74dad051e6bf459fb7
Audun Stien <auduns@stacc.com>
2021-02-15 16:41:59 +0100

    Add trailing slash to dsn

--- pkg/db/mongo.go
+++ pkg/db/mongo.go
@@ -83,7 +83,7 @@ func (ms *MongoServer) GrantPermissions() (string, error) {
 
 // Connect to Mongoserver
 func (ms *MongoServer) Connect() (string, error) {
-	url := fmt.Sprintf("mongodb://%s:%s@%s:%d?ssl=%s", ms.Username, ms.Password, ms.Host, ms.Port, ms.Ssl)
+	url := fmt.Sprintf("mongodb://%s:%s@%s:%d/?ssl=%s", ms.Username, ms.Password, ms.Host, ms.Port, ms.Ssl)
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(url))
 	if err != nil {
 		return "unable to connect to database", err
